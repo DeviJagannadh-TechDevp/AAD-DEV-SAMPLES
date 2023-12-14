5fdfc6dd1ec51bade05a6b89a5da6246cba2d6df
Devi Jagannadh Kotha <72683351+DeviJagannadh-TechDevp@users.noreply.github.com>
2023-12-14 11:30:59 +0530

    Delete AAD DEV SAMPLES directory

--- AAD DEV SAMPLES/CUSTOM CODES USING MS GRAPH FOR CRUDs/POWERSHELL/GET-MFA-INFO.ps1
+++
@@ -1,1 +0,0 @@
-﻿Get-MgReportAuthenticationMethodUserRegistrationDetail -All:$true | Select-Object UserType, UserPrincipalName, UserDisplayName, userPreferredMethodForSecondaryAuthentication, IsAdmin, IsMfaCapable, IsMfaRegistered,IsPasswordlessCapable, IsSsprCapable, IsSsprEnabled, IsSsprRegistered, MethodsRegistered, AdditionalProperties | Export-Csv "new.csv" -NoTypeInformation
